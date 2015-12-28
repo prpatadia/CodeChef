4b46ba567d9163de357edf945e721bd0ee438a56
prpatadia <pragyp@gmail.com>
2015-12-29 00:02:36 +1100

    solution ready for submission

--- Beginner/wdtbam.js
+++ Beginner/wdtbam.js
@@ -15,6 +15,9 @@ jsin.on("line",function(data){
 	data=data.replace(/\s+/g,' ');
 	if(lineNum===0){
 		numOfTestCases=data.split(' ').map(Number)[0];
+		if(numOfTestCases==0){
+			process.exit(0);
+		}
 		lineNum++;
 	}else{
 		
@@ -24,7 +27,7 @@ jsin.on("line",function(data){
 			}else if(testDataLineNum==1){
 				// reading correct answer sequence
 				testCase.correctSequence=data;
-				testDataLinuNum++;
+				testDataLineNum++;
 			}else if(testDataLineNum==2){
 				// reading chefs answer sequence
 				testCase.submittedSequence=data;
@@ -39,7 +42,6 @@ jsin.on("line",function(data){
 			}
 		
 			if(testCaseLineNum>=numOfTestCases){
-				process.stdin.close();
 				processTestCases();
 				process.exit(0);
 			}
@@ -49,9 +51,29 @@ jsin.on("line",function(data){
 
 function processTestCases(){
 	if(testSet.length==0){
+		console.log(0);
 		return;
 	}
+	var result=[];
 	for(i=0;i<testSet.length;i++){
+		correct=testSet[i].correctSequence;
+		submitted=testSet[i].submittedSequence;
+		winnings=testSet[i].winnings.split(' ').map(Number);
+		answerCount=0;
+		if(correct.length!=submitted.length || correct.length!=winnings.length-1){
+			// when user did not give proper input, return 0
+			// this is not a requirement but i felt the need to handle it
+			result.push(0);
+			continue;
+		}
+		noCorrect=true;
+		for(j=0;j<correct.length;j++){
+			if(correct.charAt(j)==submitted.charAt(j)){
+				answerCount++;
+			}
+		}
 		
+		result.push(winnings[answerCount]);
 	}
+	console.log(result.join('\n'));
 }
